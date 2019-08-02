82671bdf553f0e1172440e8498619e60f3828645
mosesKenap_94 <kvn.moseskenap@gmail.com>
2019-08-02 12:05:42 +0700

    reset password page... not until create new password

--- app/src/main/java/com/ayannah/bantenbank/screen/resetpassword/ResetPasswordActivity.java
+++ app/src/main/java/com/ayannah/bantenbank/screen/resetpassword/ResetPasswordActivity.java
@@ -97,7 +97,7 @@ public class ResetPasswordActivity extends DaggerAppCompatActivity implements Re
     @Override
     public void showSuccessSendEmail(String message) {
 
-        Intent success = new Intent(getParent(), SuccessActivity.class);
+        Intent success = new Intent(this, SuccessActivity.class);
         success.putExtra(SuccessActivity.SUCCESS_TITLE, "Kirim email Berhasil");
         success.putExtra(SuccessActivity.SUCCESS_DESC, message);
         success.putExtra(SuccessActivity.SUCCESS_COND, 1);
