b97ef3ca088f1f1cfc7c7fd8daafca8a0ee2f460
syauqi <sfajri@ayannah.com>
2019-09-16 19:11:30 +0700

    loan tenor seekbar

--- app/src/main/java/com/ayannah/asira/screen/loan/LoanFragment.java
+++ app/src/main/java/com/ayannah/asira/screen/loan/LoanFragment.java
@@ -110,6 +110,10 @@ public class LoanFragment extends BaseFragment implements LoanContract.View {
     private int minPlafond = 0;
     private int maxPlafond = 0;
 
+    //define min and max tenor var globally based on selected product loan
+    private int minTenor = 0;
+    private int maxTenor = 0;
+
     private List<String> productName;
     private ServiceProducts mServiceProducts;
     private NumberSeparatorTextWatcher plafonNumberSeparator;
@@ -203,7 +207,8 @@ public class LoanFragment extends BaseFragment implements LoanContract.View {
         installment.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                installmentTenor = (progress+1) * 6;
+//                installmentTenor = (progress+1) * 6;
+                installmentTenor = minTenor + (progress*6);
 
                 //calculate bunga
                 totalBunga = (int) (loanAmount * interest) / 100;
@@ -317,6 +322,9 @@ public class LoanFragment extends BaseFragment implements LoanContract.View {
                 @Override
                 public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
+                    installment.setProgress(0);
+                    administration = 0;
+
                     //set default loan
                     loanAmount = 0;
                     plafondCustom.setText("");
@@ -334,6 +342,12 @@ public class LoanFragment extends BaseFragment implements LoanContract.View {
                     //set min and max plafond based on selected product
                     minPlafond = serviceProducts.getProducts().get(position).getMinLoan();
                     maxPlafond = serviceProducts.getProducts().get(position).getMaxLoan();
+
+                    //set min and max tenor based on selected product
+                    minTenor = serviceProducts.getProducts().get(position).getMinTimespan();
+                    maxTenor = serviceProducts.getProducts().get(position).getMaxTimespan();
+                    int maxTenorSeekbar = ((maxTenor-minTenor)/6);
+                    installment.setMax(maxTenorSeekbar);
 
                     //set plafond sesuai dengan product yang dipilih
                     plafonMinMax.setText(String.format("Min %s - Max %s", CommonUtils.setRupiahCurrency(serviceProducts.getProducts().get(position).getMinLoan()),
@@ -383,6 +397,8 @@ public class LoanFragment extends BaseFragment implements LoanContract.View {
 
     private void CalculateData(int position, ServiceProducts serviceProducts) {
         if(!plafondCustom.getText().toString().trim().isEmpty()){
+
+            installment.setProgress(0);
 
             //get value from edittext to set plafond
             int nominal = Integer.parseInt(plafondCustom.getText().toString().replaceAll(",", ""));
